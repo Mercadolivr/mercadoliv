e052a9a702c97bb94029562d35c9ed36d1ff9c0a
Mercadolivr <159988387+Mercadolivr@users.noreply.github.com>
2024-04-01 01:58:03 -0300

    Add files via upload

---
+++ js/components-promotions.7b58fad2.js
@@ -0,0 +1,2 @@
+(self.__LOADABLE_LOADED_CHUNKS__=self.__LOADABLE_LOADED_CHUNKS__||[]).push([[5246],{99653:(_,s,A)=>{const{Promotions:D}=A(23895);_.exports=D}}]);
+//# sourceMappingURL=components-promotions.7b58fad2.js.map
